9f5a58a11b48a28ca835fc863e811031028b8264
bhugot <bhugot@gmail.com>
2018-03-15 23:51:36 +0100

    Same fix against concurrent dictionnary perf

--- src/MiniCover.HitServices/TestMethodUtils.cs
+++ src/MiniCover.HitServices/TestMethodUtils.cs
@@ -1,4 +1,4 @@
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
@@ -7,8 +7,8 @@ namespace MiniCover.HitServices
 {
     public static class TestMethodUtils
     {
-        private static readonly ConcurrentDictionary<string, bool> assemblyHasPdbCache = new ConcurrentDictionary<string, bool>();
-
+        private static readonly Dictionary<string, bool> AssemblyHasPdbCache = new Dictionary<string, bool>();
+        private static readonly object Lock = new object();
         public static MethodBase GetTestMethod()
         {
             var stackTrace = new StackTrace();
@@ -27,8 +27,14 @@ namespace MiniCover.HitServices
         private static bool HasPdb(MethodBase methodBase)
         {
             var location = methodBase.DeclaringType.Assembly.Location;
-
-            return assemblyHasPdbCache.GetOrAdd(location, l => File.Exists(Path.ChangeExtension(location, ".pdb")));
+            
+            lock (Lock)
+            {
+                if (AssemblyHasPdbCache.TryGetValue(location, out var hasPdb)) return hasPdb;
+                hasPdb = File.Exists(Path.ChangeExtension(location, ".pdb"));
+                AssemblyHasPdbCache.Add(location, hasPdb);
+                return hasPdb;
+            }
         }
     }
 }
