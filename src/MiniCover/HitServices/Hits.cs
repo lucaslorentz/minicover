1a3f8600505da6d9f208c4d6868733eaf68e6f0a
bhugot <bhugot@gmail.com>
2018-03-15 23:54:39 +0100

    removed the not needed resolver

--- src/MiniCover/HitServices/Hits.cs
+++ src/MiniCover/HitServices/Hits.cs
@@ -50,47 +50,9 @@ namespace MiniCover
 
         public static Hits ConvertToHits(string json)
         {
-            var notMergedHits = JsonConvert.DeserializeObject<HitTestMethod[]>(json, new JsonSerializerSettings {
-                    ContractResolver = new CustomContractResolver()
-                })
+            var notMergedHits = JsonConvert.DeserializeObject<HitTestMethod[]>(json)
                 .SelectMany(method => method.HitedInstructions.Select(id => new Hit(id.Key, id.Value, new[] {method}))).ToArray();
             return new Hits(notMergedHits);
-        }
-    }
-
-    public class CustomContractResolver : DefaultContractResolver {
-
-        protected override JsonObjectContract CreateObjectContract(Type objectType)
-        {
-            var c = base.CreateObjectContract(objectType);
-            if (!IsCustomStruct(objectType)) return c;
-
-            IList<ConstructorInfo> list = objectType.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).OrderBy(e => e.GetParameters().Length).ToList();
-            var mostSpecific = list.LastOrDefault();
-            if (mostSpecific != null)
-            {
-                c.OverrideCreator = CreateParameterizedConstructor(mostSpecific);
-                foreach (var constructorParameter in CreateConstructorParameters(mostSpecific, c.Properties))
-                {
-                    c.CreatorParameters.Add(constructorParameter);
-                }
-            }
-
-            return c;
-        }
-
-        protected virtual bool IsCustomStruct(Type objectType)
-        {
-            return objectType.IsValueType && !objectType.IsPrimitive && !objectType.IsEnum && !string.IsNullOrEmpty(objectType.Namespace) && !objectType.Namespace.StartsWith("System.");
-        }
-
-        private ObjectConstructor<object> CreateParameterizedConstructor(MethodBase method)
-        {
-            if(method == null) throw new ArgumentNullException(nameof(method));
-            var c = method as ConstructorInfo;
-            if (c != null)
-                return a => c.Invoke(a);
-            return a => method.Invoke(null, a);
         }
     }
 }
