68f0c5e5f8cfec51a8a421cb639f55ee61180a5e
bhugot <bhugot@gmail.com>
2018-03-18 12:09:58 +0100

    Cleanup tests from mono.cecil

--- tests/MiniCover.UnitTests/Mono/BaseTestFixture.cs
+++ tests/MiniCover.UnitTests/Mono/BaseTestFixture.cs
@@ -20,44 +20,10 @@ namespace Mono.Cecil.Tests
             return GetResourcePath(Path.Combine("assemblies", name), assembly);
         }
 
-        public static string GetCSharpResourcePath(string name, Assembly assembly)
-        {
-            return GetResourcePath(Path.Combine("cs", name), assembly);
-        }
-
-        public static string GetILResourcePath(string name, Assembly assembly)
-        {
-            return GetResourcePath(Path.Combine("il", name), assembly);
-        }
-
-        public ModuleDefinition GetResourceModule(string name)
-        {
-            return ModuleDefinition.ReadModule(GetAssemblyResourcePath(name, GetType().Assembly));
-        }
-
-        public ModuleDefinition GetResourceModule(string name, ReaderParameters parameters)
-        {
-            return ModuleDefinition.ReadModule(GetAssemblyResourcePath(name, GetType().Assembly), parameters);
-        }
-
-        public ModuleDefinition GetResourceModule(string name, ReadingMode mode)
-        {
-            return ModuleDefinition.ReadModule(GetAssemblyResourcePath(name, GetType().Assembly), new ReaderParameters(mode));
-        }
-
-        public ModuleDefinition GetCurrentModule()
-        {
-            return ModuleDefinition.ReadModule(GetType().Module.FullyQualifiedName);
-        }
-
-        public ModuleDefinition GetCurrentModule(ReaderParameters parameters)
-        {
-            return ModuleDefinition.ReadModule(GetType().Module.FullyQualifiedName, parameters);
-        }
-
         public static string FindResourcesDirectory(Assembly assembly)
         {
             var path = Path.GetDirectoryName(new Uri(assembly.CodeBase).LocalPath);
+
             while (!Directory.Exists(Path.Combine(path, "Resources")))
             {
                 var old = path;
@@ -66,14 +32,6 @@ namespace Mono.Cecil.Tests
             }
 
             return Path.Combine(path, "Resources");
-        }
-
-        public static void AssertCode(string expected, MethodDefinition method)
-        {
-            Assert.True(method.HasBody);
-            Assert.NotNull(method.Body);
-
-            Assert.Equal(Normalize(expected), Normalize(Formatter.FormatMethodBody(method)));
         }
 
         public static string Normalize(string str)
@@ -85,18 +43,6 @@ namespace Mono.Cecil.Tests
             IAssemblyResolver assemblyResolver = null, bool applyWindowsRuntimeProjections = false)
         {
             Run(new ModuleTestCase(file, test, symbolReaderProvider, assemblyResolver, applyWindowsRuntimeProjections));
-        }
-
-        public static void TestCSharp(string file, Action<ModuleDefinition> test, Type symbolReaderProvider = null,
-            IAssemblyResolver assemblyResolver = null, bool applyWindowsRuntimeProjections = false)
-        {
-            Run(new CSharpTestCase(file, test, symbolReaderProvider, assemblyResolver, applyWindowsRuntimeProjections));
-        }
-
-        public static void TestIL(string file, Action<ModuleDefinition> test, Type symbolReaderProvider = null,
-            IAssemblyResolver assemblyResolver = null, bool applyWindowsRuntimeProjections = false)
-        {
-            Run(new ILTestCase(file, test, symbolReaderProvider, assemblyResolver, applyWindowsRuntimeProjections));
         }
 
         static void Run(TestCase testCase)
@@ -138,33 +84,6 @@ namespace Mono.Cecil.Tests
         }
 
         public override string ModuleLocation => BaseTestFixture.GetAssemblyResourcePath(Module, Assembly);
-    }
-
-    class CSharpTestCase : TestCase
-    {
-
-        public readonly string File;
-
-        public CSharpTestCase(string file, Action<ModuleDefinition> test, Type symbolReaderProvider, IAssemblyResolver assemblyResolver, bool applyWindowsRuntimeProjections)
-            : base(test, symbolReaderProvider, assemblyResolver, applyWindowsRuntimeProjections)
-        {
-            File = file;
-        }
-
-        public override string ModuleLocation => CompilationService.CompileResource(BaseTestFixture.GetCSharpResourcePath(File, Assembly));
-    }
-
-    class ILTestCase : TestCase
-    {
-        public readonly string File;
-
-        public ILTestCase(string file, Action<ModuleDefinition> test, Type symbolReaderProvider, IAssemblyResolver assemblyResolver, bool applyWindowsRuntimeProjections)
-            : base(test, symbolReaderProvider, assemblyResolver, applyWindowsRuntimeProjections)
-        {
-            File = file;
-        }
-
-        public override string ModuleLocation => CompilationService.CompileResource(BaseTestFixture.GetILResourcePath(File, Assembly));
     }
 
     class TestRunner : IDisposable
--- tests/MiniCover.UnitTests/Mono/_FormatterTests.cs
+++ tests/MiniCover.UnitTests/Mono/_FormatterTests.cs
@@ -11,24 +11,31 @@ namespace Mono.Cecil.Tests
             var expectedIl = @".locals init ()
 IL_0000: ldarg.0 // this
 IL_0001: call System.Void System.Object::.ctor()
+IL_0006: nop
+IL_0007: nop
 .try
 {
-IL_0006: ldarg.0 // this
-IL_0007: ldc.i4.5
-IL_0008: stfld System.Int32 AClassWithATryFinallyInConstructor::value
-IL_000d: leave.s IL_0016
+IL_0008: nop
+IL_0009: ldarg.0 // this
+IL_000a: ldc.i4.5
+IL_000b: stfld System.Int32 Sample.TryFinally.AClassWithATryFinallyInConstructor::value
+IL_0010: nop
+IL_0011: leave.s IL_001d
 }
 finally
 {
-IL_000f: ldarg.0 // this
-IL_0010: call System.Void AClassWithATryFinallyInConstructor::Exit()
-IL_0015: endfinally
+IL_0013: nop
+IL_0014: ldarg.0 // this
+IL_0015: call System.Void Sample.TryFinally.AClassWithATryFinallyInConstructor::Exit()
+IL_001a: nop
+IL_001b: nop
+IL_001c: endfinally
 }
-IL_0016: ret";
+IL_001d: ret";
 
-            TestCSharp("Constructors.cs", module =>
+            TestModule("Sample.dll", module =>
             {
-                var type = module.GetType("AClassWithATryFinallyInConstructor");
+                var type = module.GetType("Sample.TryFinally.AClassWithATryFinallyInConstructor");
                 var constructor = type.GetMethod(".ctor");
                 Assert.NotNull(constructor);
 
