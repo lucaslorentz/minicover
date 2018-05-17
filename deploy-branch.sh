7863497017de2c0c8b133025ec4a8c09e38ebcb0
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-05-17 21:43:45 +0200

    Add back ci push to github

--- deploy-branch.sh
+++ deploy-branch.sh
@@ -8,7 +8,8 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
   export Version=$(cat version)-ci-$(date +%Y%m%d%H%M%S)
   dotnet pack -c Release --output $PWD/artifacts/ci
 
-  dotnet nuget push artifacts/ci/*.nupkg -k $MYFEED_TOKEN -s https://www.myget.org/F/minicover/api/v3/index.json
+  #dotnet nuget push artifacts/ci/*.nupkg -k $MYFEED_TOKEN -s https://www.myget.org/F/minicover/api/v3/index.json
+  dotnet nuget push artifacts/ci/*.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 else
   echo "Skipping CI deploy"
 fi
